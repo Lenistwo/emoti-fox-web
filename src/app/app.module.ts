a7da3e47b4aa6efb4bbf369f4536fb27dfd318ef
Kacper Fal <kacperek1003@gmail.com>
2020-04-03 22:01:05 +0200

    Created menu bar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RoutingModule} from './modules/routing/routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/routing/routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from '../../components/main/main.component';
+
+
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class RoutingModule { }
